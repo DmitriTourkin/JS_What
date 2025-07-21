f6c680b5ac5d5ccd99d031cca33da5cacb57d650
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-07-21 19:12:28 +0300

    Решение задач на this, call, apply

---
+++ functions/advanced_functions/decs_tasks.js
@@ -0,0 +1,48 @@
+function work(a, b) {
+  console.log(a + b);
+}
+
+function spy(fn) {
+  function wrapper(...args) {
+    let result = fn(...args);
+    wrapper.calls.push(args);
+    return result;
+  }
+
+  wrapper.calls = [];
+  return wrapper;
+}
+
+work = spy(work);
+
+work(1, 2);
+work(4, 5);
+
+for(let args of work.calls) {
+  console.log('call:' + args.join());
+}
+
+// Task 2
+function f(x) {
+  console.log(x);
+}
+
+function delay(func, ms) {
+  return function(...args) {
+    setTimeout(() => func(...args), ms);
+  }
+}
+
+// Другое решение, универсальное, учитывающее изменение контекста и аргументов
+function delayYes(func, ms) {
+  return function(...args) {
+    let savedThis = this;
+    setTimeout(() => func.apply(savedThis, args), ms)
+  }
+}
+
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000('5');
+f1500('8')
