a3c3914232d50e7ef56396ec8d0a9cff97bcc152
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-07-25 23:36:18 +0300

    Объект ошибки

--- base.js/try_catch.js
+++ base.js/try_catch.js
@@ -7,13 +7,13 @@ try {
   console.log('Ошибка возникла');
 }
 // СИНХРОННО работает: поэтому ошибки setTimeout try-catch не поймает, т.к. функция сработает «позже»
-try {
+`try {
   setTimeout(function() {
     noSuchVariable;
   }, 1000);
 } catch (e) {
   console.log('Не сработает');
-}
+}`
 
 // Чтобы поймать исключение внутри запланированной функции, 
 // try..catch должен находиться внутри самой этой функции
@@ -25,3 +25,13 @@ setTimeout(function() {
     console.log('Поймана ошибка в ходе выполнения!');
   }
 })
+
+// Объект ошибки. Структура
+// name (имя), message (сообщение о деталях), stack (стек о последовательных вложенных вызовах, для отладки),
+try {
+  lslsls;
+} catch (errObject) {
+  console.log(`name: ${errObject.name}`);
+  console.log(`message: ${errObject.message}`);
+  console.log(`stack: ${errObject.stack}`);
+}
