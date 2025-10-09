739de1513aeeeb165e01c2e2febd05ce2652340d
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-10-09 21:24:55 +0300

    Wrapper Context1, Context2

---
+++ thisApplyBindPromisesPractice/level_2.js
@@ -0,0 +1,66 @@
+// №6
+
+const { multiply } = require("lodash");
+
+const math = {
+  value: 5,
+  calculate: function(x, y) {
+    return this.value * (x + y);
+  }
+};
+
+function withLogging(fn) {
+  let boundFn = fn.bind(math);
+
+  return function(...args) {
+    return boundFn(...args);
+  }
+}
+
+math.calculate = withLogging(math.calculate);
+console.log(`math.calculate (logged): ${math.calculate(5, 6)}`);
+
+// №7
+
+const statistics = {
+  average: function() {
+    const numbers = Array.from(arguments);
+    return numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+  }
+}
+
+const averageWrapper = statistics.average;
+const data = [10, 20, 30, 40];
+
+console.log(`AW: ${averageWrapper.apply(statistics, data)}`);
+
+// №8.
+
+const greeter = {
+  template: 'Hello, {name}!',
+  greet: function(name, age) {
+    return this.template.replace('{name}', name) + ` You are ${age} years old`;
+  }
+}
+
+const clearGreet = greeter.greet;
+const halfCleanGreet = clearGreet.bind(greeter, 'John');
+console.log(halfCleanGreet(21));
+
+// №9.
+const context1 = { multiplier: 2 };
+const context2 = { multiplier: 3 };
+
+function calculate(a, b) {
+  return this.multiplier * (a + b);
+}
+
+function calculateWrapper() {
+  return function(...args) {
+    const context = this !== globalThis ? this : context1;
+    return calculate.call(context, ...args);
+  }
+}
+
+console.log("Bound Context 1 but called Context 2", calculateWrapper().call(context2, 5, 5))
+console.log("Called without call, with context", calculateWrapper()(5, 6))
